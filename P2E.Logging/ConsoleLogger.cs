e0a3ef7a45a016828d8d15f1a10cdc0c4ac38f18
Sludge Vohaul <sludge.vohaul@gmx.net>
2016-11-30 00:29:48 +0100

    Modify Logger to write out paramlist, sync writes to the Console.

--- P2E.Logging/ConsoleLogger.cs
+++ P2E.Logging/ConsoleLogger.cs
@@ -1,44 +1,47 @@
 ﻿using System;
 using System.Text;
+using System.Threading;
 using MediaBrowser.Model.Logging;
 
 namespace P2E.Logging
 {
     public class ConsoleLogger : ILogger
     {
+        private static readonly SemaphoreSlim SemSlim = new SemaphoreSlim(1, 1);
+
         public void Info(string message, params object[] paramList)
         {
-            Console.Out.WriteLine($"INFO: {message}", paramList);
+            WriteToStdOut($"INFO: {message}", paramList);
         }
 
         public void Error(string message, params object[] paramList)
         {
-            Console.Error.WriteLine($"ERROR: {message}", paramList);
+            WriteToStdErr($"ERROR: {message}", paramList);
         }
 
         public void Warn(string message, params object[] paramList)
         {
-            Console.Out.WriteLine($"WARN: {message}", paramList);
+            WriteToStdOut($"WARN: {message}", paramList);
         }
 
         public void Debug(string message, params object[] paramList)
         {
-            Console.Out.WriteLine($"DEBUG: {message}", paramList);
+            WriteToStdOut($"DEBUG: {message}", paramList);
         }
 
         public void Fatal(string message, params object[] paramList)
         {
-            Console.Error.WriteLine($"FATAL: {message}", paramList);
+            WriteToStdErr($"FATAL: {message}", paramList);
         }
 
         public void FatalException(string message, Exception exception, params object[] paramList)
         {
-            Console.Error.WriteLine($"FATALEXCEPTION: {message}", paramList);
+            WriteToStdErr($"FATALEXCEPTION: {message}", paramList);
         }
 
         public void ErrorException(string message, Exception exception, params object[] paramList)
         {
-            Console.Error.WriteLine($"ERROREXCEPTION: {message}", paramList);
+            WriteToStdErr($"ERROREXCEPTION: {message}", paramList);
         }
 
         public void LogMultiline(string message, LogSeverity severity, StringBuilder additionalContent)
@@ -49,6 +52,44 @@ namespace P2E.Logging
         public void Log(LogSeverity severity, string message, params object[] paramList)
         {
             throw new NotImplementedException();
+        }
+
+        private void WriteToStdErr(string message, params object[] paramList)
+        {
+            SemSlim.Wait();
+            try
+            {
+                if (paramList.Length == 0)
+                {
+                    Console.Error.WriteLine(message);
+                    return;
+                }
+
+                Console.Error.WriteLine($"{message}\n{{0}}", paramList);
+            }
+            finally
+            {
+                SemSlim.Release();
+            }
+        }
+
+        private void WriteToStdOut(string message, params object[] paramList)
+        {
+            SemSlim.Wait();
+            try
+            {
+                if (paramList.Length == 0)
+                {
+                    Console.Out.WriteLine(message);
+                    return;
+                }
+
+                Console.Out.WriteLine($"{message}\n{{0}}", paramList);
+            }
+            finally
+            {
+                SemSlim.Release();
+            }
         }
     }
 }
